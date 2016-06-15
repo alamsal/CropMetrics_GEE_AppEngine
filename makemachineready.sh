e508f1878279e332ba1cbfd61ec241a022669644
Lamsal <12001003026236@FEDIDCARD.GOV>
2016-06-15 17:27:47 -0600

    Fixed for windows provision

--- makemachineready.sh
+++ makemachineready.sh
@@ -4,36 +4,50 @@
 apt-get update
 
 #install python make sure python 2.6 or 2.7
-apt-get install python-pip
+apt-get -y install python-pip
 
 #install vim
-apt-get install vim
+apt-get -y install vim
 
 #Install Git
-apt-get install git
+apt-get -y install git
 
 #Openssl
-apt-get install openssl
+apt-get -y install openssl
 
 #setup Google APIs Client Library for Python provides support for authenticating to the Earth Engine servers
-pip install google-api-python-client
+sudo pip install google-api-python-client
 
 #Crypto library
-pip install pyCrypto
+sudo pip install pyCrypto
 
 #pyOpenSSL
-pip install 'pyOpenSSL>=0.11'
+sudo pip install 'pyOpenSSL>=0.11'
 
 #Google Earth Engine
-pip install earthengine-api
+sudo pip install earthengine-api
 
 #update httplib2
-pip install --upgrade httplib2
+sudo pip install --upgrade httplib2
 
 #Python library for accessing resources protected by OAuth 2.0
-pip install --upgrade oauth2client
+sudo pip install --upgrade oauth2client
+
+#download Google App Engine SDK for Python
+wget -P /var/crop_metrics/ "https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.38.zip"
+
+#Unzip App Engine SDK
+sudo unzip /var/crop_metrics/google_appengine_1.9.38.zip -d /var/crop_metrics/google_appengine1.9.38
+
+#Setup execution path
+export PATH=$PATH:/var/crop_metrics/google_appengine1.9.38/google_appengine
+
+#Copy setup project template
+sudo mkdir -p /var/crop_metrics/app/crop_app
+sudo cp -r /var/crop_metrics/google_appengine1.9.38/google_appengine/new_project_template /var/crop_metrics/app/crop_app
 
 #Setup a credentials file on your computer that authorizes access to Earth Engine on behalf of your Google account
-python -c "import ee; ee.Initialize()"
+sudo earthengine authenticate
+
 
 
