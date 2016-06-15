a709eda69d6376c0afe866d367d79db72cfdb592
Aashis Lamsal <aashislamsal@MacBook-Pro.local>
2016-06-15 00:09:08 -0600

    Update machine setup with OAuth and git

--- makemachineready.sh
+++ makemachineready.sh
@@ -4,16 +4,22 @@
 apt-get update
 
 #install python make sure python 2.6 or 2.7
-sudo apt-get install python-pip
+apt-get install python-pip
 
-#setup Google APIs Client Library for Python provides support for authenticating to the Earth Engine servers
-sudo pip install google-api-python-client
+#install vim
+apt-get install vim
 
-#Crypto library
-pip install pyCrypto
+#Install Git
+apt-get install git
 
 #Openssl
 apt-get install openssl
+
+#setup Google APIs Client Library for Python provides support for authenticating to the Earth Engine servers
+pip install google-api-python-client
+
+#Crypto library
+pip install pyCrypto
 
 #pyOpenSSL
 pip install 'pyOpenSSL>=0.11'
@@ -24,6 +30,10 @@ pip install earthengine-api
 #update httplib2
 pip install --upgrade httplib2
 
-#install vim
-apt-get install vim
+#Python library for accessing resources protected by OAuth 2.0
+pip install --upgrade oauth2client
+
+#Setup a credentials file on your computer that authorizes access to Earth Engine on behalf of your Google account
+python -c "import ee; ee.Initialize()"
+
 
