75ea09afc4b9071cb00695d26d63159be1a23240
Aashis Lamsal <aashislamsal@MacBook-Pro.local>
2016-06-15 01:08:31 -0600

    Hello World from GEE

--- setup_AppEngine_SDK_Python.sh
+++ setup_AppEngine_SDK_Python.sh
@@ -6,6 +6,11 @@ wget -P /var/crop_metrics/ "https://storage.googleapis.com/appengine-sdks/featur
 #Unzip App Engine SDK
 unzip /var/crop_metrics/google_appengine_1.9.38.zip -d google_appengine1.9.38
 
-#setup execution path
+#Setup execution path
 export PATH=$PATH:/var/crop_metrics/google_appengine1.9.38/google_appengine
 
+#Copy setup project template
+cp -r /var/crop_metrics/google_appengine1.9.38/google_appengine/new_project_template /var/crop_metrics/app/crop_app
+
+#Run test project
+python /var/crop_metrics/google_appengine1.9.38/google_appengine/dev_appserver.py /var/crop_metrics/app/crop_app/new_project_template
